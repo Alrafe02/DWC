26355654b9466827e572110cc27e53bc972d4f06
Alrafe02 <alrafe02@gmail.com>
2021-11-15 09:06:07 +0100

    15/11

---
+++ T4/Ejemplos Clase/video1.js
@@ -0,0 +1,16 @@
+function colorVerde(elem) {
+    elem.style.background = "red"
+}
+function colorBlanco(elem) {
+    elem.style.background = "white"
+}
+function cambiar1(elem) {
+    elem.innerHTML = "JavaScript en XHTML y función externa"
+}
+function alertar() {
+    alert("Vas a ir a la página web del instituto")
+}
+function confirmar() {
+    return confirm("Quieres ir a la página web del instituto?")
+}
+
