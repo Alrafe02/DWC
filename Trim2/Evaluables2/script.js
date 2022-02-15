259cb3744b0dac396e57888407433e8c07e3c327
AlbertoRF <alrafe02@gmail.com>
2022-02-15 22:50:13 +0100

    15/02

--- Trim2/Evaluables2/script.js
+++ Trim2/Evaluables2/script.js
@@ -21,7 +21,6 @@ function consultar(){
         if(this.readyState == 4 && this.status == 200) {
             
             let datos = JSON.parse(this.responseText);
-            //console.log(datos);
             let res = document.querySelector("#res");
             res.innerHTML = "";
     
